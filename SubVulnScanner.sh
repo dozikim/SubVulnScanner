0b3fd90d9dbfa5438ea8efb4df9a3fb4765d74ad
Dozikim <bs524942@gmail.com>
2024-12-23 22:11:53 +0530

    Update SubVulnScanner.sh

--- SubVulnScanner.sh
+++ SubVulnScanner.sh
@@ -2,9 +2,8 @@
 
 # Check if the script is being run with sudo privileges
 check_sudo() {
-    if [ "$(id -u)" -ne 0; then
+    if [ "$(id -u)" -ne 0 ]; then
         echo -e "\e[91m[!] This script must be run as root (with sudo).\e[0m"
-        echo -e "\e[91m[!] Please run the script with sudo.\e[0m"
         exit 1
     fi
 }
@@ -12,7 +11,7 @@ check_sudo() {
 # Banner
 print_banner() {
     echo -e "\e[95m###############################################\e[0m"
-    echo -e "\e[95m#       Subdomain Security Toolkit v3.1       #\e[0m"
+    echo -e "\e[95m#       Subdomain Vulnerability Scanner       #\e[0m"
     echo -e "\e[95m###############################################\e[0m"
 }
 
@@ -20,43 +19,10 @@ print_banner() {
 enumerate_subdomains() {
     domain=$1
     echo -e "\e[94m[+] Starting subdomain enumeration for: $domain\e[0m"
-    echo -e "\e[96mChoose tools for subdomain enumeration:\e[0m"
-    echo -e "1. Amass"
-    echo -e "2. Assetfinder"
-    echo -e "3. Subfinder"
-    echo -e "4. All tools"
-    echo -en "\e[96mYour choice: \e[0m"
-    read enum_choice
-
-    case $enum_choice in
-        1)
-            echo -e "\e[94m[+] Using Amass...\e[0m"
-            amass enum -passive -d $domain -o amass_subdomains.txt
-            cat amass_subdomains.txt > all_subdomains.txt
-            ;;
-        2)
-            echo -e "\e[94m[+] Using Assetfinder...\e[0m"
-            assetfinder --subs-only $domain > assetfinder_subdomains.txt
-            cat assetfinder_subdomains.txt > all_subdomains.txt
-            ;;
-        3)
-            echo -e "\e[94m[+] Using Subfinder...\e[0m"
-            subfinder -d $domain -o subfinder_subdomains.txt
-            cat subfinder_subdomains.txt > all_subdomains.txt
-            ;;
-        4)
-            echo -e "\e[94m[+] Using Amass, Assetfinder, and Subfinder...\e[0m"
-            amass enum -passive -d $domain -o amass_subdomains.txt
-            assetfinder --subs-only $domain > assetfinder_subdomains.txt
-            subfinder -d $domain -o subfinder_subdomains.txt
-            cat amass_subdomains.txt assetfinder_subdomains.txt subfinder_subdomains.txt | sort -u > all_subdomains.txt
-            ;;
-        *)
-            echo -e "\e[91m[!] Invalid choice. Skipping enumeration.\e[0m"
-            return
-            ;;
-    esac
-
+    amass enum -passive -d $domain -o amass_subdomains.txt
+    assetfinder --subs-only $domain > assetfinder_subdomains.txt
+    subfinder -d $domain -o subfinder_subdomains.txt
+    cat amass_subdomains.txt assetfinder_subdomains.txt subfinder_subdomains.txt | sort -u > all_subdomains.txt
     echo -e "\e[92m[+] Subdomains saved to all_subdomains.txt\e[0m"
 }
 
@@ -67,13 +33,32 @@ probe_live_subdomains() {
     echo -e "\e[92m[+] Live subdomains saved to live_subdomains.txt\e[0m"
 }
 
+# Perform SQL Injection testing
+test_sql_injection() {
+    echo -e "\e[94m[+] Testing for SQL Injection...\e[0m"
+    if [ ! -f live_subdomains.txt ]; then
+        echo -e "\e[91m[!] No live subdomains found. Please run the 'Probe Live Subdomains' option first.\e[0m"
+        return
+    fi
+
+    while read -r subdomain; do
+        if [[ ! "$subdomain" =~ ^https?:// ]]; then
+            subdomain="http://$subdomain"
+        fi
+        echo -e "\e[94m[+] Testing $subdomain for SQL Injection...\e[0m"
+        sqlmap -u "$subdomain" --batch --crawl=2 --risk=3 --level=5
+    done < live_subdomains.txt
+    echo -e "\e[92m[+] SQL Injection testing completed.\e[0m"
+}
+
 # Main menu function
 main_menu() {
     while true; do
         echo -e "\n\e[96mSelect an option:\e[0m"
         echo -e "1. Subdomain Enumeration"
         echo -e "2. Probe Live Subdomains"
-        echo -e "3. Exit"
+        echo -e "3. Test SQL Injection"
+        echo -e "4. Exit"
         echo -en "\e[96mYour choice: \e[0m"
         read choice
 
@@ -91,6 +76,9 @@ main_menu() {
                 probe_live_subdomains
                 ;;
             3)
+                test_sql_injection
+                ;;
+            4)
                 echo -e "\e[91mExiting. Goodbye!\e[0m"
                 exit 0
                 ;;
@@ -102,6 +90,6 @@ main_menu() {
 }
 
 # Run the script
-check_sudo           # Check for sudo privileges
-print_banner         # Display the banner
-main_menu            # Launch the main menu
+check_sudo
+print_banner
+main_menu
